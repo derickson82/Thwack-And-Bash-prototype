8a95f999d4d7f66599fdda9aa89cef4b22147541
Theodor <Theodor@Theodor-Dator>
2014-05-13 20:17:56 +0200

    Stuff

--- core/src/thwack/and/bash/game/Game.java
+++ core/src/thwack/and/bash/game/Game.java
@@ -19,160 +19,160 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 
 public class Game extends ApplicationAdapter{
 
-	private static OrthographicCamera gameCamera;
-	private static OrthographicCamera screenCamera;
+    private static OrthographicCamera gameCamera;
+    private static OrthographicCamera screenCamera;
 
-	private static SpriteBatch batch;
+    private static SpriteBatch batch;
 
-	private static Rectangle mapViewport;
+    private static Rectangle mapViewport;
 
-	//UI STUFF
-	private static BitmapFont font;
+    //UI STUFF
+    private static BitmapFont font;
 
-	private static TextureAtlas mainMenuAtlas;
-	private static Skin mainMenuSkin;
+    private static TextureAtlas mainMenuAtlas;
+    private static Skin mainMenuSkin;
 
-	private static ImageButtonStyle slotImageButtonStyle;
-	private static TextButtonStyle blueTextButtonStyle;
+    private static ImageButtonStyle slotImageButtonStyle;
+    private static TextButtonStyle blueTextButtonStyle;
 
-	//	public static OrthographicCamera getGameCamera(){
-	//		return gameCamera;
-	//	}
+    public static OrthographicCamera getGameCamera(){
+	return gameCamera;
+    }
 
-	public static OrthographicCamera getScreenCamera(){
-		return screenCamera;
+    public static OrthographicCamera getScreenCamera(){
+	return screenCamera;
+    }
+
+    public static SpriteBatch getBatch(){
+	return batch;
+    }
+
+    public static Rectangle getMapViewport(){
+	return mapViewport;
+    }
+
+    public static ImageButtonStyle getSlotImageButtonStyle(){
+	return slotImageButtonStyle;
+    }
+
+    public static TextButtonStyle getBlueTextButtonStyle(){
+	return blueTextButtonStyle;
+    }
+
+    public static BitmapFont getFont(){
+	return font;
+    }
+
+    private static GameScreen screen;
+    private static GameScreen lastScreen;
+    private static GameScreen nextScreen;
+
+    public static void setScreen(GameScreen screen){
+	if(Game.screen != null){
+	    Game.screen.dispose();
+	    Game.lastScreen = Game.screen;
 	}
+	Game.screen = screen;
+	Game.screen.show();
+    }
 
-	public static SpriteBatch getBatch(){
-		return batch;
-	}
+    public static void startLastScreen(){
+	setScreen(lastScreen);
+    }
 
-	public static Rectangle getMapViewport(){
-		return mapViewport;
-	}
+    public static void setNextScreen(GameScreen nextScreen){
+	Game.nextScreen = nextScreen;
+    }
 
-	public static ImageButtonStyle getSlotImageButtonStyle(){
-		return slotImageButtonStyle;
-	}
+    public static void startNextScreen(){
+	setScreen(nextScreen);
+	nextScreen = null;
+    }
 
-	public static TextButtonStyle getBlueTextButtonStyle(){
-		return blueTextButtonStyle;
-	}
+    public static GameScreen getCurrentScreen(){
+	return screen;
+    }
 
-	public static BitmapFont getFont(){
-		return font;
-	}
+    public static void clearScreen(){
+	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
 
-	private static GameScreen screen;
-	private static GameScreen lastScreen;
-	private static GameScreen nextScreen;
+    public static void setClearColor(Color color){
+	Gdx.gl.glClearColor(color.r, color.g, color.b, color.a);
+    }
 
-	public static void setScreen(GameScreen screen){
-		if(Game.screen != null){
-			Game.screen.dispose();
-			Game.lastScreen = Game.screen;
-		}
-		Game.screen = screen;
-		Game.screen.show();
-	}
+    public static float getWidth(){
+	return screenCamera.viewportWidth;
+    }
 
-	public static void startLastScreen(){
-		setScreen(lastScreen);
-	}
-
-	public static void setNextScreen(GameScreen nextScreen){
-		Game.nextScreen = nextScreen;
-	}
-
-	public static void startNextScreen(){
-		setScreen(nextScreen);
-		nextScreen = null;
-	}
-
-	public static GameScreen getCurrentScreen(){
-		return screen;
-	}
-
-	public static void clearScreen(){
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-	}
-
-	public static void setClearColor(Color color){
-		Gdx.gl.glClearColor(color.r, color.g, color.b, color.a);
-	}
-
-	public static float getWidth(){
-		return screenCamera.viewportWidth;
-	}
-
-	public static float getHeight(){
-		return screenCamera.viewportHeight;
-	}
+    public static float getHeight(){
+	return screenCamera.viewportHeight;
+    }
 
 
-	@Override
-	public void create(){
-		batch = new SpriteBatch();
+    @Override
+    public void create(){
+	batch = new SpriteBatch();
 
-		screenCamera = new OrthographicCamera();
-		screenCamera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+	screenCamera = new OrthographicCamera();
+	screenCamera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
-		mapViewport = new Rectangle(1f / 8f * getWidth(), 1f / 8f * getHeight(), 6f / 8f * getWidth(), 6f / 8f * getHeight());
+	mapViewport = new Rectangle(1f / 8f * getWidth(), 1f / 8f * getHeight(), 6f / 8f * getWidth(), 6f / 8f * getHeight());
 
-		gameCamera = new OrthographicCamera();
-		gameCamera.setToOrtho(false, getWidth(), getHeight());
+	gameCamera = new OrthographicCamera();
+	gameCamera.setToOrtho(false, getWidth(), getHeight());
 
-		font = new BitmapFont(Gdx.files.internal("fonts/font.fnt"));
+	font = new BitmapFont(Gdx.files.internal("fonts/font.fnt"));
 
 
-		mainMenuAtlas = new TextureAtlas(Gdx.files.internal("textureatlas/UI/output/mainMenuAtlas.atlas"));
-		mainMenuSkin = new Skin(mainMenuAtlas);
+	mainMenuAtlas = new TextureAtlas(Gdx.files.internal("textureatlas/UI/output/mainMenuAtlas.atlas"));
+	mainMenuSkin = new Skin(mainMenuAtlas);
 
-		blueTextButtonStyle = new TextButtonStyle();
-		blueTextButtonStyle.down = mainMenuSkin.getDrawable("BlueButtonDown");
-		blueTextButtonStyle.over = mainMenuSkin.getDrawable("BlueButtonOver");
-		blueTextButtonStyle.up = mainMenuSkin.getDrawable("BlueButtonUp");
-		blueTextButtonStyle.font = font;
-		blueTextButtonStyle.fontColor = Color.BLACK;
+	blueTextButtonStyle = new TextButtonStyle();
+	blueTextButtonStyle.down = mainMenuSkin.getDrawable("BlueButtonDown");
+	blueTextButtonStyle.over = mainMenuSkin.getDrawable("BlueButtonOver");
+	blueTextButtonStyle.up = mainMenuSkin.getDrawable("BlueButtonUp");
+	blueTextButtonStyle.font = font;
+	blueTextButtonStyle.fontColor = Color.BLACK;
 
-		slotImageButtonStyle = new ImageButtonStyle();
-		slotImageButtonStyle.up = mainMenuSkin.getDrawable("BlueButtonUp");
-		slotImageButtonStyle.over = mainMenuSkin.getDrawable("BlueButtonOver");
-		slotImageButtonStyle.disabled = mainMenuSkin.getDrawable("BlueButtonDown");
+	slotImageButtonStyle = new ImageButtonStyle();
+	slotImageButtonStyle.up = mainMenuSkin.getDrawable("BlueButtonUp");
+	slotImageButtonStyle.over = mainMenuSkin.getDrawable("BlueButtonOver");
+	slotImageButtonStyle.disabled = mainMenuSkin.getDrawable("BlueButtonDown");
 
-		setScreen(new SplashScreen());
-	}
+	setScreen(new SplashScreen());
+    }
 
-	@Override
-	public void render(){
-		clearScreen();
+    @Override
+    public void render(){
+	clearScreen();
 
-		gameCamera.update();
-		screenCamera.update();
-		batch.setProjectionMatrix(screenCamera.combined);
+	gameCamera.update();
+	screenCamera.update();
+	batch.setProjectionMatrix(screenCamera.combined);
 
-		screen.update(Gdx.graphics.getDeltaTime());
-		screen.render(batch);
-	}
+	screen.update(Gdx.graphics.getDeltaTime());
+	screen.render(batch);
+    }
 
-	@Override
-	public void dispose(){
-		screen.dispose();
-	}
+    @Override
+    public void dispose(){
+	screen.dispose();
+    }
 
-	@Override
-	public void resume(){
-		screen.resume();
-	}
+    @Override
+    public void resume(){
+	screen.resume();
+    }
 
-	@Override
-	public void pause(){
-		screen.pause();
-	}
+    @Override
+    public void pause(){
+	screen.pause();
+    }
 
-	@Override
-	public void resize(int width, int height){
-		screen.resize(width, height);
-	}
+    @Override
+    public void resize(int width, int height){
+	screen.resize(width, height);
+    }
 
 }
--- core/src/thwack/and/bash/game/level/Level.java
+++ core/src/thwack/and/bash/game/level/Level.java
@@ -22,154 +22,155 @@ import com.badlogic.gdx.physics.box2d.World;
 
 public class Level {
 
-	private Level(){
-	}
+    private Level(){
+    }
 
-	private static TiledMap tiledMap;
-	private static OrthogonalTiledMapRenderer mapRenderer;
+    private static TiledMap tiledMap;
+    private static OrthogonalTiledMapRenderer mapRenderer;
 
-	public static void render(){
-		Rectangle r = Game.getMapViewport();
-		Gdx.gl.glViewport((int)r.x, (int)r.y, (int)r.width, (int)r.height);
- 		mapRenderer.setView(Game.getScreenCamera());
-		mapRenderer.render();
-		Gdx.gl.glViewport(0, 0, (int)Game.getWidth(), (int)Game.getHeight());
-	}
+    public static void render(){
+	Rectangle r = Game.getMapViewport();
+	Gdx.gl.glViewport((int)r.x, (int)r.y, (int)r.width, (int)r.height);
+	Game.getGameCamera().update();
+	mapRenderer.setView(Game.getGameCamera());
+	mapRenderer.render();
+	Gdx.gl.glViewport(0, 0, (int)Game.getWidth(), (int)Game.getHeight());
+    }
 
-	public static void load(String tmxLevel, World world){
-		tiledMap = new TmxMapLoader().load("levels/" + tmxLevel);
-		mapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
-		addBox2D(world);
-	}
+    public static void load(String tmxLevel, World world){
+	tiledMap = new TmxMapLoader().load("levels/" + tmxLevel);
+	mapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+	addBox2D(world);
+    }
 
-	private static void addBox2D(World world){
-		float tileSize = 0;
-		ArrayList<Box2DCell> cells = new ArrayList<Box2DCell>();
-		MapLayer mapLayer = tiledMap.getLayers().get("collision");
-		TiledMapTileLayer layer = (TiledMapTileLayer) mapLayer;
-		tileSize = layer.getTileWidth();
-		for(int y = 0; y < layer.getHeight(); y++){
-			for(int x = 0; x < layer.getWidth(); x++){
-				Cell cell = layer.getCell(x, y);
-				if(cell == null){
-					continue;
-				}
-				if(cell.getTile() == null){
-					continue;
-				}
-
-				cells.add(new Box2DCell(cell, x, y));
-			}
+    private static void addBox2D(World world){
+	float tileSize = 0;
+	ArrayList<Box2DCell> cells = new ArrayList<Box2DCell>();
+	MapLayer mapLayer = tiledMap.getLayers().get("collision");
+	TiledMapTileLayer layer = (TiledMapTileLayer) mapLayer;
+	tileSize = layer.getTileWidth();
+	for(int y = 0; y < layer.getHeight(); y++){
+	    for(int x = 0; x < layer.getWidth(); x++){
+		Cell cell = layer.getCell(x, y);
+		if(cell == null){
+		    continue;
+		}
+		if(cell.getTile() == null){
+		    continue;
 		}
 
-		//X ___
-		//  l   J
-		//   ----
-		int i = cells.size() - 1;
-		ArrayList<Box2DCell> yCells = new ArrayList<Box2DCell>();
-		while(true){
-			Box2DCell c = cells.get(0);
-			float x = c.x;
-			while(true){
-				Box2DCell c2 = findCellToRight(cells, c, x);
-				if(c2 != null){
-					cells.remove(c2);
-					x++;
-					i--;
-				} else {
-					break;
-				}
-			}
-			float x2 = x - c.x + 1;
-			if(x2 - 1 >= 1){
-				BodyDef bodyDef = new BodyDef();
-				bodyDef.position.x = Pixels.toMeters(c.x * tileSize + x2 * tileSize / 2);
-				bodyDef.position.y = Pixels.toMeters(c.y * tileSize + tileSize / 2);
-				bodyDef.type = BodyType.StaticBody;
-				Body body = world.createBody(bodyDef);
-				PolygonShape shape = new PolygonShape();
-				shape.setAsBox(Pixels.toMeters(x2 * tileSize) / 2, Pixels.toMeters(tileSize) / 2);
-				FixtureDef fixtureDef = new FixtureDef();
-				fixtureDef.shape = shape;
-				fixtureDef.friction = 0;
-				body.createFixture(fixtureDef);
-			}
-			if(x2 - 1 == 0){
-				yCells.add(c);
-			}
-			cells.remove(c);
-			if(cells.size() <= 0){
-				break;
-			}
-		}
-
-		//Y
-		// _
-		//l  l
-		//l  l
-		//L _J
-		while(true){
-			Box2DCell c = yCells.get(0);
-			float y = c.y;
-			while(true){
-				Box2DCell c2 = findCellToDown(yCells, c, y);
-				if(c2 != null){
-					yCells.remove(c2);
-					y++;
-				} else {
-					break;
-				}
-			}
-			float y2 = y - c.y + 1;
-			if(y2 == 0){
-				y2 = 1;
-			}
-			BodyDef bodyDef = new BodyDef();
-			bodyDef.position.x = Pixels.toMeters(c.x * tileSize + tileSize / 2);
-			bodyDef.position.y = Pixels.toMeters(c.y * tileSize + y2 * tileSize / 2);
-			bodyDef.type = BodyType.StaticBody;
-			Body body = world.createBody(bodyDef);
-			PolygonShape shape = new PolygonShape();
-			shape.setAsBox(Pixels.toMeters(tileSize) / 2, Pixels.toMeters(y2 * tileSize) / 2);
-			FixtureDef fixtureDef = new FixtureDef();
-			fixtureDef.shape = shape;
-			fixtureDef.friction = 0;
-			body.createFixture(fixtureDef);
-			yCells.remove(c);
-			if(yCells.size() <= 0){
-				break;
-			}
-		}
-
+		cells.add(new Box2DCell(cell, x, y));
+	    }
 	}
 
-	private static Box2DCell findCellToRight(ArrayList<Box2DCell> cells, Box2DCell c, float x){
-		for(int i = 0; i < cells.size(); i++){
-			if(cells.get(i) == c){
-				continue;
-			}
-			if(cells.get(i).y == c.y && cells.get(i).x == x + 1){
-				Box2DCell c2 = cells.get(i);
-				cells.remove(i);
-				return c2;
-			}
+	//X ___
+	//  l   J
+	//   ----
+	int i = cells.size() - 1;
+	ArrayList<Box2DCell> yCells = new ArrayList<Box2DCell>();
+	while(true){
+	    Box2DCell c = cells.get(0);
+	    float x = c.x;
+	    while(true){
+		Box2DCell c2 = findCellToRight(cells, c, x);
+		if(c2 != null){
+		    cells.remove(c2);
+		    x++;
+		    i--;
+		} else {
+		    break;
 		}
-		return null;
+	    }
+	    float x2 = x - c.x + 1;
+	    if(x2 - 1 >= 1){
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.position.x = Pixels.toMeters(c.x * tileSize + x2 * tileSize / 2);
+		bodyDef.position.y = Pixels.toMeters(c.y * tileSize + tileSize / 2);
+		bodyDef.type = BodyType.StaticBody;
+		Body body = world.createBody(bodyDef);
+		PolygonShape shape = new PolygonShape();
+		shape.setAsBox(Pixels.toMeters(x2 * tileSize) / 2, Pixels.toMeters(tileSize) / 2);
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = shape;
+		fixtureDef.friction = 0;
+		body.createFixture(fixtureDef);
+	    }
+	    if(x2 - 1 == 0){
+		yCells.add(c);
+	    }
+	    cells.remove(c);
+	    if(cells.size() <= 0){
+		break;
+	    }
 	}
 
-	private static Box2DCell findCellToDown(ArrayList<Box2DCell> cells, Box2DCell c, float y){
-		for(int i = 0; i < cells.size(); i++){
-			if(cells.get(i) == c){
-				continue;
-			}
-			if(cells.get(i).y == y + 1 && cells.get(i).x == c.x){
-				Box2DCell c2 = cells.get(i);
-				cells.remove(i);
-				return c2;
-			}
+	//Y
+	// _
+	//l  l
+	//l  l
+	//L _J
+	while(true){
+	    Box2DCell c = yCells.get(0);
+	    float y = c.y;
+	    while(true){
+		Box2DCell c2 = findCellToDown(yCells, c, y);
+		if(c2 != null){
+		    yCells.remove(c2);
+		    y++;
+		} else {
+		    break;
 		}
-		return null;
+	    }
+	    float y2 = y - c.y + 1;
+	    if(y2 == 0){
+		y2 = 1;
+	    }
+	    BodyDef bodyDef = new BodyDef();
+	    bodyDef.position.x = Pixels.toMeters(c.x * tileSize + tileSize / 2);
+	    bodyDef.position.y = Pixels.toMeters(c.y * tileSize + y2 * tileSize / 2);
+	    bodyDef.type = BodyType.StaticBody;
+	    Body body = world.createBody(bodyDef);
+	    PolygonShape shape = new PolygonShape();
+	    shape.setAsBox(Pixels.toMeters(tileSize) / 2, Pixels.toMeters(y2 * tileSize) / 2);
+	    FixtureDef fixtureDef = new FixtureDef();
+	    fixtureDef.shape = shape;
+	    fixtureDef.friction = 0;
+	    body.createFixture(fixtureDef);
+	    yCells.remove(c);
+	    if(yCells.size() <= 0){
+		break;
+	    }
 	}
+
+    }
+
+    private static Box2DCell findCellToRight(ArrayList<Box2DCell> cells, Box2DCell c, float x){
+	for(int i = 0; i < cells.size(); i++){
+	    if(cells.get(i) == c){
+		continue;
+	    }
+	    if(cells.get(i).y == c.y && cells.get(i).x == x + 1){
+		Box2DCell c2 = cells.get(i);
+		cells.remove(i);
+		return c2;
+	    }
+	}
+	return null;
+    }
+
+    private static Box2DCell findCellToDown(ArrayList<Box2DCell> cells, Box2DCell c, float y){
+	for(int i = 0; i < cells.size(); i++){
+	    if(cells.get(i) == c){
+		continue;
+	    }
+	    if(cells.get(i).y == y + 1 && cells.get(i).x == c.x){
+		Box2DCell c2 = cells.get(i);
+		cells.remove(i);
+		return c2;
+	    }
+	}
+	return null;
+    }
 
 }
 
