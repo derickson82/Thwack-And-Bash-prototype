0d433cdc389adf0ab1441f1e2700d4396bfb18ae
unknown <jemtan@gmail.com>
2014-06-08 16:37:40 -0400

    Initial collision detection logic based on raycasting

---
+++ core/src/thwack/and/bash/game/collision/SnakeGuard.java
@@ -0,0 +1,57 @@
+package thwack.and.bash.game.collision;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.RayCastCallback;
+
+public class SnakeGuard implements RayCastCallback {
+
+	private static final int NEAREST = -1;
+	/**
+	 * LOS = Line of sight
+	 * Collision = current collision point
+	 * Normal = a line perpendicular to the collision point
+	 */
+	Vector2 startLOS = new Vector2(), endLOS = new Vector2(), collision = new Vector2(), normal = new Vector2();
+
+	public SnakeGuard(Vector2 startLOS, Vector2 endLOS) {
+		this.startLOS = startLOS;
+		this.endLOS = endLOS;
+	}
+
+	public Vector2 getStartLOS() {
+		return startLOS;
+	}
+
+	public void setStartLOS(Vector2 startLOS) {
+		this.startLOS = startLOS;
+	}
+
+	public Vector2 getEndLOS() {
+		return endLOS;
+	}
+
+	public void setEndLOS(Vector2 endLOS) {
+		this.endLOS = endLOS;
+	}
+
+	public Vector2 getCollision() {
+		return collision;
+	}
+
+	public Vector2 getNormal() {
+		return normal;
+	}
+
+	@Override
+	public float reportRayFixture(Fixture fixture, Vector2 point,
+			Vector2 normal, float fraction) {
+		
+		collision.set(point);
+		System.out.println("collision point = [" + collision.x + "," + collision.y + "]");
+		SnakeGuard.this.normal.set(normal).add(point);
+		
+		return NEAREST;
+	}
+
+}
---
+++ core/src/thwack/and/bash/game/test/RendererDebug.java
@@ -0,0 +1,10 @@
+package thwack.and.bash.game.test;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public interface RendererDebug {
+
+	//just this one for now
+	public void render(SpriteBatch batch);
+
+}
