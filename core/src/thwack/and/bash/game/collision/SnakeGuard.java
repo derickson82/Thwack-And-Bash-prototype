2702096dad48786a70fd6993618b5df667fcc738
unknown <jemtan@gmail.com>
2014-06-12 17:25:04 -0400

    Refactored snake guard so that its logic can be replaced out easily

---
+++ core/src/thwack/and/bash/game/collision/SnakeGuard.java
@@ -0,0 +1,21 @@
+package thwack.and.bash.game.collision;
+
+import com.badlogic.gdx.math.Vector2;
+
+import thwack.and.bash.game.entity.Entity;
+
+public interface SnakeGuard {
+
+    public Entity hit(float x, float y);
+
+	public Vector2 getEndLOS();
+
+	public void setStartLOS(Vector2 start);
+
+	public Object getNormal();
+
+	public Vector2 getStartLOS();
+
+	public Object getCollision();
+
+}
