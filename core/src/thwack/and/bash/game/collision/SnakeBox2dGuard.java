f4bdfb885b68a6764e399919f8c88e71a37f8383
unknown <jemtan@gmail.com>
2014-06-14 07:08:25 -0400

    Added missing files

---
+++ core/src/thwack/and/bash/game/collision/SnakeBox2dGuard.java
@@ -0,0 +1,96 @@
+package thwack.and.bash.game.collision;
+
+import thwack.and.bash.game.entity.Entity;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.Manifold;
+
+public class SnakeBox2dGuard implements SnakeGuard, ContactListener {
+
+	private SnakeCollisionHelper collide = new SnakeCollisionHelper();
+	private boolean playerNearby;
+	
+	public boolean isPlayerNearby() {
+		return playerNearby;
+	}
+
+	public SnakeBox2dGuard(Vector2 foreHeadPos, Vector2 furthestFrontSightPos) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void beginContact(Contact contact) {
+		if(collide.isPlayer(contact)) {
+	        Gdx.app.log(this.getClass().getName() + " beginContact", "between player " + collide.getFixturePlayer().getUserData() + " and " + collide.getFixtureHit().getUserData());
+	        if(collide.getFixtureHit().getUserData().equals(SnakeCollisionHelper.SNAKE_ID)) {
+	        	playerNearby = true;
+		        Gdx.app.log(this.getClass().getName(), " attack player !");
+	        }
+		}
+	}
+
+	@Override
+	public void endContact(Contact contact) {
+		if(collide.isPlayer(contact)) {
+	        Gdx.app.log(this.getClass().getName() + " endContact", "between player " + collide.getFixturePlayer().getUserData() + " and " + collide.getFixtureHit().getUserData());
+	        if(collide.getFixtureHit().getUserData().equals(SnakeCollisionHelper.SNAKE_ID)) {
+	        	playerNearby = false;
+		        Gdx.app.log(this.getClass().getName(), " continue");
+	        }
+		}
+	}
+
+	@Override
+	public void preSolve(Contact contact, Manifold oldManifold) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void postSolve(Contact contact, ContactImpulse impulse) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Entity hit(float x, float y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vector2 getEndLOS() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setStartLOS(Vector2 start) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object getNormal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Vector2 getStartLOS() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object getCollision() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ core/src/thwack/and/bash/game/collision/SnakeCollisionHelper.java
@@ -0,0 +1,46 @@
+package thwack.and.bash.game.collision;
+
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.Fixture;
+
+public class SnakeCollisionHelper {
+
+	public static final String PLAYER_ID = "player";
+	public static final String SNAKE_ID = "snake";
+	public static final String TILE_ID = "tile";
+	public static final String BAT_ID = "bat";
+    Fixture fixturePlayer;
+    Fixture fixtureUnknown;
+
+	public Fixture getFixturePlayer() {
+		return fixturePlayer;
+	}
+
+	public Fixture getFixtureHit() {
+		return fixtureUnknown;
+	}
+
+	public boolean isPlayer(Contact contact) {
+		boolean ret = false;
+		
+		Fixture fixtureA = contact.getFixtureA();
+		Fixture fixtureB = contact.getFixtureB();
+
+		if(fixtureA.getBody().getUserData() != null) {
+			if(fixtureA.getBody().getUserData().equals(SnakeCollisionHelper.PLAYER_ID)) {
+				ret = true;
+				fixturePlayer = fixtureA;
+				fixtureUnknown = fixtureB;
+			}
+		}
+		if(fixtureB.getBody().getUserData() != null) {
+			if(fixtureB.getBody().getUserData().equals(SnakeCollisionHelper.PLAYER_ID)) {
+				ret = true;
+				fixturePlayer = fixtureB;
+				fixtureUnknown = fixtureA;
+			}
+		}
+
+		return ret;
+	}
+}
