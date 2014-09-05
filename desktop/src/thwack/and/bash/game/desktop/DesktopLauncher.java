f77aff899c1e7136ceb74a82e779babcc163db2b
jamestan <xxx@yyy.mail.com>
2014-09-04 20:40:42 -0400

    Added KISS setup guide in DesktopLauncher

--- desktop/src/thwack/and/bash/game/desktop/DesktopLauncher.java
+++ desktop/src/thwack/and/bash/game/desktop/DesktopLauncher.java
@@ -17,6 +17,12 @@ public class DesktopLauncher {
 		config.vSyncEnabled = false;
 //		new LwjglApplication(new Game(), config);
 		/**
+		 * Setup:
+		 * 
+		 * 1. Set your sdk.dir in local.properties like sdk.dir=C:/adt-bundle-windows-x86_64-20140702/sdk
+		 * 2. Hit "gradlew eclipse"
+		 * 3. Point Run Configuration's Argument "Working directory" Other to ${workspace_loc:Thwack and Bash-android/assets}
+		 * 
 		 * You have two options to make this work:
 		 * 1. Comment it (the code below) out and use the real Game class (the code above)!
 		 * 2. Add the "test/" folder and use it as a source folder :)
